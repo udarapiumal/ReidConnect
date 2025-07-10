731936e7667e77aa1734b404325d28bd01361a49
ChathuraHS <chathurahemanthe@gmail.com>
2025-07-11 02:27:49 +0530

    minor commit

--- backend/src/main/java/reidConnect/backend/dto/PostUpdateDto.java
+++ backend/src/main/java/reidConnect/backend/dto/PostUpdateDto.java
@@ -12,14 +12,10 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 
-<<<<<<<< Updated upstream:backend/src/main/java/reidConnect/backend/dto/PostUpdateDto.java
 public class PostUpdateDto {
-========
-public class PostResponseDto {
     private Long id;
     private Long clubId;
     private String title;
->>>>>>>> Stashed changes:backend/src/main/java/reidConnect/backend/dto/PostResponseDto.java
     private String description;
     private List<String> mediaPaths;
 }
