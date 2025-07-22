61ca922a6e8dfe4f94af030e8ae9646c50dc15c2
ChathuraHS <chathurahemanthe@gmail.com>
2025-07-22 15:42:29 +0530

    minor changes

--- frontend/constants/config.ts
+++ frontend/constants/config.ts
@@ -1,4 +1,4 @@
-const LOCAL_IP = "192.168.1.2"; // Changed to localhost for browser logging
+const LOCAL_IP = "192.168.246.83"; // Changed to localhost for browser logging
 
 export const BASE_URL = `http://${LOCAL_IP}:8080`;
 
